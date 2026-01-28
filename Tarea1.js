a11e2ad5969fe4744a1fc95f84ac6bd026c47b8e
Masuelg-Pineda <jostin.masuelg.pineda@gmail.com>
2026-01-27 20:46:26 -0600

    Tarea 1 terminada

---
+++ Tarea1.js
@@ -0,0 +1,30 @@
+// Ejercicio 1  --> 42
+let numero = 42;
+console.log(numero);
+console.log(typeof numero);
+
+// Ejercicio 2 --> veinticinco
+let nombre = "veinticinco";
+console.log(nombre);
+console.log(typeof nombre);
+
+// Ejercicio 3 --> -666
+console.log(typeof -666);
+
+// Ejericio 4
+console.log(typeof true);
+
+// Ejericio 5
+console.log(typeof 0);
+
+// Ejericio 6
+console.log(typeof "");
+
+// Ejericio 7
+console.log(typeof null);
+
+// Ejericio 8
+console.log(typeof undefined);
+
+// Ejericio 9
+console.log(typeof FALSE);
