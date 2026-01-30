4ee73ae55804820929207e5c934a5563cc6c23ac
Masuelg-Pineda <jostin.masuelg.pineda@gmail.com>
2026-01-29 19:09:14 -0600

    Primera media hora de clase

---
+++ Class2.js
@@ -0,0 +1,39 @@
+//Operadores basicos
+var color = "azul";
+var color = "verde";
+var color = "morado";
+
+console.log(color); //lEE DE ARRIBA HACIA ABAJO, OSEA IMPRIME LA ULTIMA
+
+var color1 = "azul";
+var color2 = "verde";
+var color3 = "morado";
+
+console.log(color1);
+console.log(color2);
+console.log(color3);
+
+let mascota = "perro";
+let mascota1 = "gato";
+
+const numero = 23;
+
+
+let num1 = 3;
+let num2 = 5;
+let suma = num1 + num2;
+let resta = num2 - num1;
+
+console.log(suma);
+console.log(15+15);
+console.log(resta);
+
+console.log(num1*num2);
+console.log(num2/num1);
+
+console.log(num1 == num2);
+console.log(10 == 10);
+
+console.log(12 == '12'); //dato numerico igual a dato string (el mismo)
+console.log(23 === '23'); //comparación estricta
+
