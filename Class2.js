f395d2ca041c177749e03c02080322ff8511e83f
Masuelg-Pineda <jostin.masuelg.pineda@gmail.com>
2026-01-29 19:27:23 -0600

    Cambios 19:27hrs

--- Class2.js
+++ Class2.js
@@ -18,22 +18,44 @@ let mascota1 = "gato";
 
 const numero = 23;
 
-
+console.log("Operadores matematicos");
 let num1 = 3;
 let num2 = 5;
 let suma = num1 + num2;
 let resta = num2 - num1;
+let modulo = num1 % num2;
 
 console.log(suma);
 console.log(15+15);
 console.log(resta);
+console.log(modulo);
 
 console.log(num1*num2);
 console.log(num2/num1);
 
+
+console.log("Operadores logicos");
+//comparacion simple
 console.log(num1 == num2);
 console.log(10 == 10);
-
 console.log(12 == '12'); //dato numerico igual a dato string (el mismo)
+
+//comparación estricta
 console.log(23 === '23'); //comparación estricta
 
+console.log("----------------------------");
+let valor1 = 20;
+let valor2 = 30;
+
+//Mayor que
+console.log(valor1 > valor2);
+//Mayor o igual que
+console.log(valor1 >= valor2);
+//Menor que
+console.log(valor1 < valor2);
+//Menor o igual que
+console.log(valor1 <= valor2);
+//Distinto que
+console.log(10 != 10);
+//Distinto que estricto
+console.log(10 !== 10);
