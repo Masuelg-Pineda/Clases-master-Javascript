c583a108138b394fdb5ef57afd4159f3e691237e
Masuelg-Pineda <jostin.masuelg.pineda@gmail.com>
2026-02-05 19:20:36 -0600

    CLASE 05FEB2026

---
+++ funciones.js
@@ -0,0 +1,30 @@
+/* function nombreDescriptivo(parametros){
+    //Lo que va a hacer mi funcion
+    return "Hola" + parametros
+}
+*/
+function saludar(nombre){
+    //Lo que va a hacer mi funcion
+    return "Hola " + nombre;
+}
+
+saludar("Ana"); //Llamando a mi funcion saludar
+
+let saludo = saludar ("Fatima");
+
+console.log(saludar("Felipe"));
+console.log(saludo);
+
+function sumar(a,b){
+    console.log (a + b); //Resultado en consola
+}
+
+sumar(2,3);
+
+function restar(num1, num2){
+    let resultado = num1 - num2;
+    return resultado;
+}
+
+console.log(restar(10,7));
+
