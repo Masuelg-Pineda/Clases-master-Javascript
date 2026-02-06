3405215888492aa041c2ab7bcf51d49d7e09b7ba
Masuelg-Pineda <jostin.masuelg.pineda@gmail.com>
2026-02-05 19:50:41 -0600

    funciones en javascript avance clase

--- funciones.js
+++ funciones.js
@@ -21,10 +21,55 @@ function sumar(a,b){
 
 sumar(2,3);
 
+//return entrega un valor para usarlo despues
 function restar(num1, num2){
     let resultado = num1 - num2;
-    return resultado;
+    return resultado; //Esta variable es local
 }
 
 console.log(restar(10,7));
 
+let resultado; // esta es variable global
+
+function sinreturn (){
+    let x = 5;
+}
+//logra ejecutar la funcion, pero no regresa nada
+console.log(sinreturn()); //undefined
+
+
+function multiplicar(dato1, dato2){
+    return dato1 * dato2;
+}
+
+console.log(multiplicar(4)); //Not a Number NaN, hace falta pasarle un argumento
+
+console.log(Ana); //error de variable no definida
+
+//arrow function o funcion de flecha
+
+const dividir = (a,b) => a/b; //return automatico
+console.log(dividir(15,2));
+
+const suma = (a,b) => { //una funcion mas larga {} no tiene return automatico
+    return a + b;
+}
+
+console.log(suma(3,4));
+
+const multi = (a,b) => {return a * b}
+console.log(multi(10, 5));
+
+//funciones anonimas para callbacks, eventos frontend
+
+//funcion anonima (no necesita darle nombre, ejecuta algo puntual)
+//Callbacks
+setTimeout(function(){
+    console.log("Esto se ejecuta despues...")
+}, 5000);
+
+//Cuando no ocupar funciones anónimas
+/*Se tiene que reutilizar muchas veces
+tiene logica grande o compleja
+necesita ser testeada
+Se necesita claridad */
