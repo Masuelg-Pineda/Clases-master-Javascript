185ebd7c08a1dcffd9ee0bfb7c729d2c4c8e2902
Masuelg-Pineda <jostin.masuelg.pineda@gmail.com>
2026-02-10 19:37:47 -0600

    Continuacion clase metodos

--- objetos.js
+++ objetos.js
@@ -10,6 +10,28 @@ const disco = {
     //Propiedad del tipo arreglo
     canciones: ["NuevaYol", "Mojaita", "Un x100to", "Tití me preguntó", "La Mudanza"],
 
-    //Podemos guardar acciones relacionadas con los datos
-
+    //Podemos guardar ACCIONES relacionadas con los datos
+    //Metodo que muestra la informacion
+    mostrarInfo() {
+        console.log(`${this.titulo} de ${this.artista}, lanzado en ${this.anio}`);
+        //this apunta a ESTE objeto (disco)
+    }
+    //Metodo que liste las canciones
+    mostrarCanciones(){
+        this.canciones.forEach((cancion, i)) => {
+            console.log(`${i + 1 } ${cancion}`);
+        }
+    }
 }
+
+//Acceder a los datos con notacion de punto
+console.log(disco.artista);
+console.log(disco.canciones);
+
+//Acceder a los datos con notacion de corchetes
+console.log(disco["titulo"]);
+console.log(disco["dispobible"]);
+
+//llamar a nuestro método
+disco.mostrarInfo();
+disco.mostrarCanciones();
