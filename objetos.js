2931232d60e3ea6601b91a379e23d73892dcc087
Masuelg-Pineda <jostin.masuelg.pineda@gmail.com>
2026-02-10 19:04:17 -0600

    Comienzo de clase 10feb26

---
+++ objetos.js
@@ -0,0 +1,15 @@
+//Un objeto es una colección de pares clave - valor (propiedades)
+
+const disco = {
+    //Propiedades (datos del disco) atributos del objeto
+    titulo: "Debi tirar mas fotos",
+    artista: "Bad Bunny",
+    anio: "2025",
+    dispobible: true,
+
+    //Propiedad del tipo arreglo
+    canciones: ["NuevaYol", "Mojaita", "Un x100to", "Tití me preguntó", "La Mudanza"],
+
+    //Podemos guardar acciones relacionadas con los datos
+
+}
