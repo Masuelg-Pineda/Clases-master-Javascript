e1beb220877c57e1aff350787240bf98f7a68f32
Masuelg-Pineda <jostin.masuelg.pineda@gmail.com>
2026-02-12 20:03:57 -0600

    Se guardan cambios

--- mains.js
+++ mains.js
@@ -9,6 +9,7 @@ console.log(texto);
 const caja = document.getElementById("caja");
 console.log(caja);
 
+const bTn = document.getElementById("bTn");
 const bTnTexto = document.getElementById("bTnTexto");
 const bTnColor = document.getElementById("bTnColor");
 const bTnOcultar = document.getElementById("bTnOcultar");
@@ -16,11 +17,25 @@ const bTnOcultar = document.getElementById("bTnOcultar");
 //Cambiar texto
 
 bTnTexto.addEventListener("click", () => {
-    titulo.textContent = "Cambiaste el texto desde Javascript";
+    titulo.textContent = "Cambiaste el texto desde Javascript de la clase de hoy";
 });
 
-//Cambiar color
+//Cambiar colores
 bTnColor.addEventListener("click", () => {
-    titulo.style.color = "white";
-    titulo.style.backgroundColor = "coral";
+    titulo.style.color = "blue";
+    caja.style.backgroundColor = "blue";
+});
+
+//Para ocultar mi caja
+bTnOcultar.addEventListener("click", () => {
+    if (caja.style.display === "none") {
+        caja.style.display = "block";
+    } else {
+        caja.style.display = "none";
+    }
+});
+
+//Cambiar de clase
+bTn.addEventListener("click", () => {
+    caja.classList.toggle("activa"); //Cambia el estado visual
 });
